0d0e067957f670e6dbbdf89f6ba1cee3e99be43c
Jarby Agudelo <jarby1211@icloud.com>
2015-11-28 12:43:05 -0500

    Index.php

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+/*
+Updates Schedule System Version: 1.0.0
+By Jarby Agudelo
+Copyright (c) 2015
+Based on the mtdowling/cron-expression
+Copyright (c) 2009-2012
+Updates Schedule System, like mtdowling/cron-expression V1.0.3, is 100% free and open-source.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Updates Schedule System">
+    <meta name="author" content="Jarby Agudelo">
+    <meta name="robots" content="noindex, nofollow">
+
+    <title>Updates Schedule System</title>
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    
+    
+    <!-- Custom CSS -->
+<link href="assets/css/schedule.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+    <div class="container">
+    <!-- Header -->
+    <div class="header"> 
+      <br/><i class="fa fa-calendar fa-2x fa-border"></i>
+      </div><br/>
+      <div class="jumbotron">
+        <center><h1>Welcome to <span class="red">Updates</span>
+		 <span class="black"> Schedule System!</h1></span></center>
+        <center><p class="lead"> Use Cron Expressions to Calculate the next or previous run date and determine if a CRON expression is due</p></center>
+		<div class="row">
+			<div class="col-sm-12">
+			  <center><a href="schedule.php" class="btn btn-danger btn-lg" role="button">Click here to Begin</a></center>
+			</div>
+        </div>
+      </div>
+    </div>
+     </header>
+     
+     
+        
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
